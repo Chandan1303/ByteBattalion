40514633f4f5f7e58be7870b4f567e5735c83169
Chandan1303 <166271180+Chandan1303@users.noreply.github.com>
2025-07-05 14:32:31 +0530

    Update App.jsx

--- social-impact-project/frontend/src/App.jsx
+++ social-impact-project/frontend/src/App.jsx
@@ -1,1 +1,19 @@
+import React,{useContext} from "react";
+import Quiz from "./components/Quiz";
+import Result from "./components/Result";
+import Tips from"./components/Tips";
+import {QuizContext} from "./context/QuizContext";
 
+
+export default function App(){
+  const {showResult} = useContext(QuizContext);
+
+  return (
+    <div className="app-container">
+      <h1 className="title">🌟 GreenQuiz 🌟</h1>
+      {!showResult && <Tips/>}
+      {showResult ? <Result/> : <Quiz /> }
+    
+    </div>
+    );
+}
