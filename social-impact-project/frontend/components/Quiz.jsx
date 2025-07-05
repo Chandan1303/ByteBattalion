ee53f126076cf6d5252c71a407c6fb7373c51e56
Chandan1303 <166271180+Chandan1303@users.noreply.github.com>
2025-07-05 14:55:58 +0530

    Add files via upload

---
+++ social-impact-project/frontend/components/Quiz.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Question from "./Question";
+
+export default function Quiz() {
+  return (
+    <div className="quiz-container">
+      <Question />
+    </div>
+  );
+}
