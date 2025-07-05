f3400cdc380f97e4b41d4231b994cd5d85169847
Chandan1303 <166271180+Chandan1303@users.noreply.github.com>
2025-07-05 14:22:40 +0530

    Update main.jsx

--- social-impact-project/frontend/src/main.jsx
+++ social-impact-project/frontend/src/main.jsx
@@ -1,1 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import {QuizProvider} from "./context/QuizContext";
+import "./index.css";
+import "./App.css";
 
+
+ReactDOM.createRoot(document.getElementByID("root")).render(
+  <React.StrictMode>
+    <QuizProvider>
+      <App/>
+    
+    </QuizProvider>
+  
+  </React.StrictMode>
+);
