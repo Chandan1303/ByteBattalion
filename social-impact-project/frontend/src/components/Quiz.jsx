fd7ffa4f03ec661ebd44772ab048d9d802b1a98c
Chandan1303 <166271180+Chandan1303@users.noreply.github.com>
2025-07-06 15:03:32 +0530

    Update Quiz.jsx

--- social-impact-project/frontend/src/components/Quiz.jsx
+++ social-impact-project/frontend/src/components/Quiz.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Question from "./Question";
 
+
 export default function Quiz() {
-  return (
-    <div className="quiz-container">
-      <Question />
-    </div>
-  );
+    return (
+        <div className="quiz-container">
+            <Question />
+        </div>
+    );
 }
